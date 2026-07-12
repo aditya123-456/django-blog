abb4bd3893bdbae90f2a7cc16d065f02817d34af
Aditya <your-github-email@example.com>
2026-07-12 19:13:39 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,20 @@
-
-
 from django.shortcuts import render
+from blogs.models import Category, Blog
+import random
 
 def home(request):
-    return render(request,'home.html')
+    categories = Category.objects.all()
+
+    featured_posts = Blog.objects.filter(is_featured=True)
+    featured_post = random.choice(list(featured_posts)) if featured_posts.exists() else None
+
+    posts = Blog.objects.filter(status='published', is_featured=False).order_by('-created_at')
+
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'featured_post': featured_post,
+        'posts': posts,
+    }
+
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog, Category
 
-# Create your views here.
+def post_by_category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+
+    posts = Blog.objects.filter(
+        category=category,
+        status='published'
+    )
+
+    context = {
+        'category': category,
+        'posts': posts,
+    }
+
+    return render(request, 'post_by_category.html', context)
